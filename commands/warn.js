5cec1b856aea1cd3506dd549c61fab18c75b10a0
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-06 18:37:32 -0800

    Update warn command since I broke it

--- commands/warn.js
+++ commands/warn.js
@@ -7,9 +7,10 @@ module.exports = {
     const client = new Discord.Client();
     const fs = require('fs');
     const userid = message.mentions.users.first().id
-    const reason = args.join(' ')
+    let reasonraw = args.filter(arg => !Discord.MessageMentions.USERS_PATTERN.test(arg));
+    const reason = reasonraw.join(' ')
     const authorusername = message.author.username
-    fs.appendFileSync('./logs/' + userid + '-warnings.log', '\nWarning\: Issued by'+  +'\nReason\: ');
+    fs.appendFileSync('./logs/' + userid + '-warnings.log', '\nWarning\: Issued by '+ authorusername +'\nReason\:' + reason);
     message.channel.send('<@'+message.mentions.users.first().id + '> had a warn logged.')
-  }
+    }
     };
