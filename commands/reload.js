2521a1fa95d6993fa174af5a5574dd29f29bf073
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-13 18:54:41 -0700

    Added command reload

---
+++ commands/reload.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'Reload',
+    description: 'Reloads the whole bot for quick updates of the code',
+    execute(message, args) {
+        try {message.channel.send('Bye! :wave:');
+        process.kill()}catch(error) {
+			// Your code broke (Leave untouched in most cases)
+			const fs = require('fs');
+			const Discord = require('discord.js');
+			const { MessageEmbed } = require('discord.js')
+			var today = new Date();
+			var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
+			var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+			global.dateTime = date+' '+time;
+			fs.appendFileSync('./debuglogs/'+sessionid+'-error.log','('+dateTime+')'+error+'\n\n');
+			console.error('an error has occured', error);
+		  }
+    },
+};
