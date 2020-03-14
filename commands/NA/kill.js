fa378a8f149bdc3f625c9c55417af1ef234cb246
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-13 18:49:11 -0700

    Removed broken kill command

--- commands/NA/kill.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'Reload',
-    description: 'Reloads the whole bot for quick updates of the code',
-    execute(message, args) {
-        message.channel.send('Shutting down...');
-        process.kill()
-    },
-};
