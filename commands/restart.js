69f9786b2831daef38b91254c667ad76258ed630
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-15 12:34:48 -0700

    Ok HOPEFULLY finally fixed restart command

--- commands/restart.js
+++ commands/restart.js
@@ -7,11 +7,9 @@ module.exports = {
 	execute(message, args) {
 		if (message.member.roles.cache.some(role => role.name === 'Bot Manager')) {
 		message.channel.send('Restarting. See you soon! :wave:')
-		const readyforrestartcheck = [ ]
-		readyforrestartcheck.push('yes')
-		if (readyforrestartcheck.includes = 'yes'){
+		setTimeout(function(){ 
 			process.exit()
-		}
+		}, 3000);
 
 	}else {
 		message.reply(`you don't seem to have the correct permissions to use this command. Please try again later or contact the bot owner.`)
