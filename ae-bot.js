ce3a761abc8e5872282fe4f96b6626c1c3ed0eef
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-15 15:00:33 -0700

    Update ae-bot.js

--- ae-bot.js
+++ ae-bot.js
@@ -109,14 +109,16 @@ client.on('error', error => {
 
 //Member join
 client.on('guildMemberAdd', member => {
-	let role = member.guild.roles.find(r => r.name === "Member");
-	member.addRole(role).catch(console.error);
+
+
 	var today = new Date();
 	var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	global.dateTime = date+' '+time;
 	const channel = client.guild.channels.cache.find(ch => ch.id === '612473686665986048');
-	const guild = member.guild
+	const guild = member.guild	
+	let role = guild.roles.find(r => r.name === "Member");	
+	member.addRole(role).catch(console.error);
 	if (!channel) return;
 
 	const MemberJoinEmbed = new Discord.MessageEmbed()
