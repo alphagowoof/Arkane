34602da3ef419926019b7abd8bd86c99d6e06820
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-15 14:26:14 -0700

    Removed the extra stuff in the restart command

--- commands/restart.js
+++ commands/restart.js
@@ -9,21 +9,6 @@ module.exports = {
 		const { MessageEmbed } = require('discord.js')
 		if (message.member.roles.cache.some(role => role.name === 'Bot Manager')) {
 		message.channel.send('Restarting. See you soon! :wave:')
-		var today = new Date();
-	var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
-	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-	var dateTime = date+' '+time;
-		const RestartEmbed = new Discord.MessageEmbed()
-		.setColor('#FFA500')
-		.setTitle('Bot Restart')
-		.setDescription('Bot is currently restarting. Please wait a moment.')
-		.addFields(
-			{ name: 'Current date/time(PST): ', value: dateTime, inline: true },
-		)
-		.setTimestamp()
-		.setFooter('Bot written by Daniel C');
-	const botlogchannel = client.channels.cache.get('688834736554246158');
-	botlogchannel.send('Restart initiated by' + message.author.user.tag)
 		setTimeout(function(){ 
 			process.exit()
 		}, 5000);
