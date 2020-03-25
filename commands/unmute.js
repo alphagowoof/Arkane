406b8c0b63a513af356c90daea98b739a2c343d4
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-25 02:49:35 -0700

    Fix muted command

--- commands/unmute.js
+++ commands/unmute.js
@@ -9,11 +9,11 @@ module.exports = {
     const Discord = require('discord.js');
     const client = new Discord.Client();
     const fs = require('fs');
-    const MuteRoleID = require('../info.json');
+    const {MuteRoleID} = require('../info.json');
     try {const reason = args.join(' ')
     const taggeduser = message.mentions.users.first().id
     const guild = message.guild
-    const role = guild.roles.cache.find(role => role.id === MuteRoleID);
+    const role = guild.roles.cache.find(role => role.id === `${MuteRoleID}`);
     const member = message.mentions.members.first();
    member.roles.remove(role);
     message.channel.send('<@'+ taggeduser +'> was unmuted.');
