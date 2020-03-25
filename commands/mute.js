102523962135627c311cf4ca2ab0e0a637b69a23
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-25 02:47:18 -0700

    Fixed mute command role

--- commands/mute.js
+++ commands/mute.js
@@ -5,21 +5,23 @@ module.exports = {
   usage: '<user> <reason>',
   cooldown: 0,
   mod:true,
+  nodelay:true,
 	execute(message, args) {
     const Discord = require('discord.js');
     const client = new Discord.Client();
     const fs = require('fs');
-    const MuteRoleID = require('../info.json');
+    const {MuteRoleID} = require('../info.json');
+    console.log("some string")
     const {MutedString, MutedStringDM} = require('../strings.json');
     try {
       let reasonraw = args.filter(arg => !Discord.MessageMentions.USERS_PATTERN.test(arg));
       const reason = reasonraw.join(' ')
      const taggeduser = message.mentions.members.first().id
      const guild = message.guild
-     const role = guild.roles.cache.find(role => role.id === MuteRoleID);
+     const role = guild.roles.cache.find(role => role.id === `${MuteRoleID}`);
      const mentionedmember = '<@'+message.mentions.users.first().id+'>'
       const member = message.mentions.members.first();
-     member.roles.add(role);
+     member.roles.add([role]);
      member.send(`You were muted due to:\n ${reason}`)
      message.channel.send(mentionedmember+' was muted.');
       fs.appendFileSync('./logs/' + taggeduser + '-warnings.log', 'Mute\nReason: ' + reason +'\n\n');
