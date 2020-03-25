7ddaff364cd77e67a688d0c21130e669cab3a82c
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-25 12:56:42 -0700

    Added coinflip and roll dice commands

---
+++ commands/coinflip.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'coinflip',
+  aliases: ['flipcoin'],
+  description: 'Flips a coin.',
+  usage: '',
+  cooldown: 0,
+	execute(message, args) {
+    const Discord = require('discord.js');
+    const client = new Discord.Client();
+    const fs = require('fs');
+    try {
+    var number = Math.ceil(Math.random() * 10)
+    if (number > 5){
+      message.channel.send('Heads.')
+    return;
+  }else {
+    message.channel.send('Tails.')}
+    return;
+  }catch(error) {
+    // Your code broke (Leave untouched in most cases)
+    console.error('an error has occured', error);
+    }
+  }}
