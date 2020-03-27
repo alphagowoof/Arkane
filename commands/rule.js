ef8ffa7052aab191e175604a53a0e3e5a42ee5dc
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-27 01:42:43 -0700

    Fixed and enabled rule  command

--- commands/rule.js
+++ commands/rule.js
@@ -4,24 +4,21 @@ module.exports = {
   description: 'Sends a rule.',
   usage: '<rule number>',
   cooldown: 0,
-  disable:true,
+  disable:false,
 	execute(message, args) {
     const Discord = require('discord.js');
     const client = new Discord.Client();
 	const fs = require('fs');
-
-	//loading rules
-	//const {Rule1, Rule2, Rule3, Rule4, Rule5, Rule6, Rule7, Rule8, Rule9, Rule10} = require('../rules.json')
 	const rulenumber = args.join(' ')
-	fs.readFile('../files/rules/rule'+rulenumber, (err, data) => {
-		const ruledata = data
-	try {
-		 if (ruledata == 'undefined')return;
+	//loading rules, thanks to 🍗 for helping me add the below part
+	var entry = rulenumber; // This is the argument of the command ".load 1"
+	var dict = require("../rules.json"); // The JSON data to load from
+	var result = dict['Rule'+entry]; // The thing that was loaded
+	if(`${result}` == 'undefined'){message.channel.send('Please make sure you entered a valid rule number.');return;}
+		try {
 		const RuleEmbed = new Discord.MessageEmbed()
-		.setTitle('About Rule')
-		.addFields(
-		  { name: 'Rule', value: `${ruledata}`, inline: false },
-			  )
+		.setTitle('About Rule '+args)
+		.setDescription(`${result}`)
 		.setTimestamp()
 		message.channel.send(RuleEmbed);
 		return;
@@ -29,6 +26,6 @@ module.exports = {
 			// Your code broke (Leave untouched in most cases)
 			console.error('an error has occured', error);
 		  }
-		})
+		
 		  
   }}
