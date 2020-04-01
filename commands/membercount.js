ea497478cb89990931f21b2bf767758225bf7c9f
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-31 23:48:30 -0700

    Added server icon to membercount command

--- commands/membercount.js
+++ commands/membercount.js
@@ -13,6 +13,7 @@ module.exports = {
 			const MemberCountEmbed = new Discord.MessageEmbed()
 	.setTitle('Server Member Count')
 	.setAuthor('Requested by '+message.member.user.tag)
+	.setThumbnail(`${message.guild.iconURL()}`)
 	.addFields(
 		{ name: message.guild.name + '\'s member count', value: `${message.guild.memberCount}`, inline: false },
 	)
