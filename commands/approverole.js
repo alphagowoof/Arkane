3cc6ecc89470f4462704033b38f9cc6b7d0e10a8
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-16 16:15:58 -0700

    You can now assign roles with `.approve`

---
+++ commands/approverole.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: 'approverole',
+  aliases: ['approve', 'assignrole'],
+  description: 'Approves a role to a user.',
+  usage: '',
+  cooldown: 0,
+	execute(message, args, client) {
+   try{
+    const Discord = require('discord.js');
+    const rolename = args.filter(arg => !Discord.MessageMentions.USERS_PATTERN.test(arg)).join(' ')
+    const role = message.guild.roles.cache.find(role => role.name === rolename);
+    const member = message.mentions.members.first();
+    member.roles.add([role]).catch(error => {
+    respond('Error', 'Something went wrong.\n'+error, message.channel)
+    return;
+    })
+    respond('✅ Role Approved', `<@${message.mentions.members.first().id}> had the \`${rolename}\` role approved.`, message.channel)
+  }catch(error) {
+    respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+    errorlog(error)
+    // Your code broke (Leave untouched in most cases)
+    console.error('an error has occured', error);
+    }
+  }}
