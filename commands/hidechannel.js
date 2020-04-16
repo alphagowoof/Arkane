26c6c076408a868e7f70268088410627b049bbbb
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-16 12:10:10 -0700

    Update hidechannel.js

--- commands/hidechannel.js
+++ commands/hidechannel.js
@@ -4,16 +4,17 @@ module.exports = {
   description: 'Hides the channel the command is ran in.',
   cooldown: 0,
   mod:true,
-	execute(message, args) {
+	execute(message, args, client) {
 	const channel = message.channel
     try {
 		channel.updateOverwrite(channel.guild.roles.everyone, { VIEW_CHANNEL: false });
 		respond('','<#'+message.channel+'> was hidden.',message.channel)
 		modaction(this.name, message.author.tag, message.channel.name, message.content)
-	}
-		catch(error) {
-			// Your code broke (Leave untouched in most cases)
-			console.error('an error has occured', error);
-		  }
+	}catch(error) {
+		respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+		errorlog(error)
+		// Your code broke (Leave untouched in most cases)
+		console.error('an error has occured', error);
+		}
 		  
   }}
