f29a537e07d10fd5f4ec75c491f6c58353639cae
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-16 12:10:02 -0700

    Update showchannel.js

--- commands/showchannel.js
+++ commands/showchannel.js
@@ -5,16 +5,17 @@ module.exports = {
   usage: '',
   cooldown: 0,
   mod:true,
-	execute(message, args) {
+	execute(message, args, client) {
 	const channel = message.channel
     try {
 		channel.updateOverwrite(channel.guild.roles.everyone, { VIEW_CHANNEL: true });
 		respond('','<#'+message.channel.id+'> is no longer hidden.', message.channel)
 		modaction(this.name, message.author.tag, message.channel.name, message.content)
-	}
-		catch(error) {
-			// Your code broke (Leave untouched in most cases)
-			console.error('an error has occured', error);
-		  }
+	}catch(error) {
+		respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+		errorlog(error)
+		// Your code broke (Leave untouched in most cases)
+		console.error('an error has occured', error);
+		}
 		  
   }}
