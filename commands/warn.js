f3df0d741752b57756a22963b334e5b5b460d3d4
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-16 17:14:50 -0700

    Fix warn

--- commands/warn.js
+++ commands/warn.js
@@ -13,19 +13,15 @@ module.exports = {
       if (message.author.id == message.mentions.members.first().id){message.channel.send(`You can't perform this action on yourself.`);return;}
       const {ModeratorRoleID} = require('../info.json');
 			const checkmemberforroles = message.mentions.members.first()
-			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){
-        message.channel.send(`You can't perform that action on this user.`);return;
-      }
+			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){message.channel.send(`You can't perform that action on this user.`);return;}
       const userid = message.mentions.users.first().id
       const mentionedmember = '<@'+message.mentions.users.first().id+'>'
       const reasonraw = args.filter(arg => !Discord.MessageMentions.USERS_PATTERN.test(arg));
       var reason = reasonraw.join(' ')
       const authorusername = message.author.username +'#' +message.author.discriminator + ` (${message.author.id}) `
-      if(reason == ''){
-        var reason = 'No reason provided.'
-      }
+      if(reason == ''){var reason = 'No reason provided.'}
       fs.appendFileSync('./logs/' + userid + '-warnings.log', 'Warning\nReason: ' + reason +'\n\n');
-      fs.appendFileSync('./logs/' + userid + '-modwarnings.log',`Warning issued by ${authorusername}: \nReason: ${reason}\n\n`);
+      fs.appendFileSync('./logs/' + userid + '-modwarnings.log',`Warning issued by ${authorusername}: \nReason: + ${reason}\n\n`);
       respond('⚠️',mentionedmember + ' had a warning logged.\nReason: '+reason, message.channel)
       const warnedperson = message.mentions.users.first()
       const user = client.users.cache.get(warnedperson);
