162fc6aeb082bd4715a43449041973197e1ae3a1
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-25 17:47:16 -0700

    Removed sanity check from clearwarn command

--- commands/clearwarning.js
+++ commands/clearwarning.js
@@ -2,28 +2,44 @@ module.exports = {
   name: 'clearwarnings',
   aliases: ['clearwarn', 'clearlog'],
   description: 'Erases punishment log.',
-  usage: '<user> <SANITY CHECK>',
+  usage: '<user>',
   cooldown: 0,
   mod:true,
 	execute(message, args, client) {
     const fs = require('fs');
     try {
-      if (message.author.id == message.mentions.members.first().id){message.channel.send(`You can't perform this action on yourself.`);return;}
+      if (message.author.id == message.mentions.members.first().id){
+        respond('',`You can't perform this action on yourself.`, message.channel);return;
+      }
       const {ModeratorRoleID} = require('../info.json');
 			const checkmemberforroles = message.mentions.members.first()
-			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){message.channel.send(`You can't perform that action on this user.`);return;}
-      var sanitycheck = args.join(' ')
-      if(!sanitycheck.includes('YES I AM SURE')){
-        respond('','Sanity check failed. Please type `YES I AM SURE` at the end of the message to allow. Please note this action is **permanent** and can\'t be undone.',message.channel)}else{
-          fs.unlink('./logs/' + message.mentions.members.first().id + '-warnings.log',function(error){
+			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){
+        respond('',`You can't perform that action on this user.`, message.channel);return;
+      }
+      if (fs.existsSync('./logs/' + message.mentions.members.first().id + '-warnings.log')
+          &&
+          fs.existsSync('./logs/' + message.mentions.members.first().id + '-modwarnings.log')){
+
+        fs.unlink('./logs/' + message.mentions.members.first().id + '-warnings.log',function(error){
+          if(error){
             console.log(error);
+            respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+            errorlog(error)
+          }
+          
         });
-          fs.unlink('./logs/' + message.mentions.members.first().id + '-modwarnings.log',function(error){
-           console.log(error);
-        });
+        fs.unlink('./logs/' + message.mentions.members.first().id + '-modwarnings.log',function(error){
+          if(error){
+            console.log(error);
+            respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+            errorlog(error)
+          }
+        })
         respond('✏️','<@'+message.mentions.members.first().id + '> had their punishment log cleared.', message.channel)
         modaction(this.name, message.author.tag, message.channel.name, message.content)
-        }
+      }else{
+        respond('❌', 'Unable to find punishment information for this user.', message.channel)
+      }
       }catch(error) {
         respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
         errorlog(error)
