93334ec532a32019cdcc62ae1968d3e129c2fa77
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-04-25 17:53:27 -0700

    Updated unmute command to use embeds

--- commands/unmute.js
+++ commands/unmute.js
@@ -11,10 +11,10 @@ module.exports = {
     const fs = require('fs');
     const {MuteRoleID} = require('../info.json');
     try {
-      if (message.author.id == message.mentions.members.first().id){message.channel.send(`You can't perform this action on yourself.`);return;}
+      if (message.author.id == message.mentions.members.first().id){respond('',`You can't perform this action on yourself.`, message.channel);return;}
       const {ModeratorRoleID} = require('../info.json');
 			const checkmemberforroles = message.mentions.members.first()
-			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){message.channel.send(`You can't perform that action on this user.`);return;}
+			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){respond('',`You can't perform that action on this user.`, message.channel);return;;return;}
     const reason = args.join(' ')
     const taggeduser = message.mentions.users.first().id
     const guild = message.guild
