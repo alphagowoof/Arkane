15a433679fdb0ddb2391096e9b97d55a1488f170
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-02 15:49:41 -0700

    Fixed up ban command and now logs in audit logs

--- commands/ban.js
+++ commands/ban.js
@@ -11,8 +11,9 @@ module.exports = {
 			const {ModeratorRoleID} = require('../info.json');
 			const checkmemberforroles = message.mentions.members.first()
 			if (checkmemberforroles.roles.cache.some(role => role.id === `${ModeratorRoleID}`)){respond('',`You can't perform that action on this user.`, message.channel);return;;return;}
-			const user = message.mentions.users.first();
-			const userid = user.id
+			const user = message.mentions.members.first();
+			const userToBan = message.mentions.members.first()
+			const userid = message.mentions.members.first().id
 			const guild = message.guild
 			const authorusername = message.author.username +'#' +message.author.discriminator
 			let reasonraw = args.filter(arg => !Discord.MessageMentions.USERS_PATTERN.test(arg));
@@ -20,9 +21,9 @@ module.exports = {
 			if(reason == ''){var reason = 'No reason provided.'}
 			fs.appendFileSync('./logs/' + userid + '-warnings.log', 'Ban\nReason: ' + reason +'\n\n');
    			fs.appendFileSync('./logs/' + userid + '-modwarnings.log', 'Ban issued by '+ authorusername +'\nReason: ' + reason +'\n\n');
-			respond('Ban','<@'+user+'> was banned.\nReason: '+reason, message.channel)
-			respond('Banned','You were banned from the Apple Explained server due to: '+ reason, user)
-			guild.members.ban(user);
+			respond('Ban','<@'+userid+'> was banned.\nReason: '+reason, message.channel)
+			respond('Banned','You were banned from the Apple Explained server due to: '+ reason+'\n\nThis ban does not expire. ', user)
+			userToBan.ban({reason: reason})
 			modaction(this.name, message.author.tag, message.channel.name, message.content)
         	}catch(error) {
 				respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
