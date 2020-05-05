faff1bddbe7416b9d77e526ffc4d5f612d172a93
Tommy Stefanos <64721643+tommystefanatic@users.noreply.github.com>
2020-05-05 16:48:09 +0800

    Finally finished game commands
    All bugs are fixed, I added more questions to the .trivia command. This command is ready for release, if TechGeekGamer approves.

--- commands/NORMAL_answer.js
+++ commands/NORMAL_answer.js
@@ -1,16 +1,33 @@
 module.exports = {
-    name: 'answer',
+    name: "answer",
     aliases: [''],
-    description: 'Answer a trivia question.\n(Run .trivia first before using .answer)',
-    usage: '(Question ID) (Letter of answer) | .answer 4 D',
+    description: "Answer a trivia question.\n(Run .trivia first before using .answer)",
+    usage: "(Question ID) (Letter of answer) | .answer 4 D",
     cooldown: 0,
       execute(message, args, client) {
       const Discord = require('discord.js');
       const fs = require('fs');
+      const quiz = require('./quiz.json');
+      const arg = message.content.slice('').trim().split(/ +/g); 
+      console.log(arg[1] + ", " + arg[2])
+      var modified = arg[1] - 1
+      const item = quiz[modified]
+      console.log(item)
+      const ans = item.answer
+      console.log(ans)
+      console.log(arg[2])
+      const filter = response => {
+        console.log(response)
+        return item.answer
+      };
       try {
-        const args = message.content.slice('.answer').trim().split(/ +/g); 
-        console.log(args[1] + "," + args[2])
-        message.channel.send('This command is still under testing!')
+        if(arg[2] == item.answer) {
+            console.log('answer correct')
+            respond('', "That was the correct answer!", message.channel)
+        }else if(arg[2] != item.answer) {
+            console.log('answer wrong')
+            respond('', "That was the wrong answer", message.channel)
+        }
   }catch(error) {
       respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
       errorlog(error)
--- commands/NORMAL_trivia.js
+++ commands/NORMAL_trivia.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'trivia',
   aliases: ['factgame', 'triviagame'],
-  description: 'Learn something new with Apple Mod.\n(Trivia will only include history of the Apple company, the Apple Explained channel and server for now.)\nTo answer a question, use .trivia(question ID) (your answer).',
+  description: 'Learn something new with Apple Mod.\n(Trivia will only include history of the Apple company, the Apple Explained channel, the server and its users for now.)\nTo answer a question, use .answer (question ID) (your answer).',
   usage: '',
   cooldown: 0,
 	execute(message, args, client) {
@@ -10,7 +10,7 @@ module.exports = {
     const msg = message
     try {
     //Initialize variables
-    var qa = Math.ceil(Math.random() * 13)
+    var qa = Math.ceil(Math.random() * 25)
     //Pick a question
     const quiz = require('./quiz.json');
     const item = quiz[Math.floor(Math.random() * quiz.length)];
@@ -18,8 +18,8 @@ module.exports = {
     const filter = response => {
       console.log(response)
       return item.answer
-    };
-        message.channel.send(`<@${message.author.id}>'s question:\n` + item.question+ '\n' + item.choice1 + '\n' + item.choice2 +'\n' + item.choice3 + '\n' + item.choice4 + '\n' + "Question ID: " + item.qid + '\n' + "**use `.answer (question ID) (your answer)` to answer the question**\n Example: .answer 2 C") 
+    }
+        message.channel.send(`<@${message.author.id}>'s question:\n` + item.question+ '\n' + item.choice1 + '\n' + item.choice2 +'\n' + item.choice3 + '\n' + item.choice4 + '\n' + "Question ID: " + item.qid + '\n' + "**use `.answer (question ID) (your answer)` to answer the question**\nExample: .answer 2 C") 
   }catch(error) {
     respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
     errorlog(error)
