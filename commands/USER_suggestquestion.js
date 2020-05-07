0701ee73c7ee010fc3c9c7381039718b2f5956f6
Tommy Stefanos <64721643+tommystefanatic@users.noreply.github.com>
2020-05-07 15:16:49 +0800

    Decreased suggestquestion cooldown to 3 mins

--- commands/USER_suggestquestion.js
+++ commands/USER_suggestquestion.js
@@ -3,13 +3,16 @@ module.exports = {
     aliases: ['triviaadd', 'addtrivia', 'addquestion', 'questionadd'],
     description: 'Suggests a question for the .trivia command. (Question only!)',
     usage: '(question)',
-    cooldown: 300,
+    cooldown: 180,
       execute(message, args, client) {
       const Discord = require('discord.js');
       const fs = require('fs');
       const argarray = message.content.slice().trim().split(/ +/g);
       const text = args.join(' ');
       try {
+        if(text = undefined) {
+          respond('No question found','Type a question after typing ' + argarray[0] + ".", message.channel)
+        }
         respond('Suggestion sent!','Your question has been sent to the developer of .trivia and is now under review!', message.channel, '29BF00')
         respond('New question suggestion', 'A question have been sent for your review.\n' + text, message.guild.members.cache.get('454579681602043916'), 'FFFFFF')
         message.delete()
