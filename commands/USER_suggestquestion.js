40d6bbebc70640bec3456ddd76675ebd45552cbf
Tommy Stefanos <64721643+tommystefanatic@users.noreply.github.com>
2020-05-07 13:42:40 +0800

    Some modifications to suggestquestion, and fixed rules.json

--- commands/USER_suggestquestion.js
+++ commands/USER_suggestquestion.js
@@ -3,6 +3,7 @@ module.exports = {
     aliases: ['triviaadd', 'addtrivia', 'addquestion', 'questionadd'],
     description: 'Suggests a question for the .trivia command. (Question only!)',
     usage: '(question)',
+    cooldown: 300,
       execute(message, args, client) {
       const Discord = require('discord.js');
       const fs = require('fs');
@@ -10,7 +11,7 @@ module.exports = {
       const text = args.join(' ');
       try {
         respond('Suggestion sent!','Your question has been sent to the developer of .trivia and is now under review!', message.channel)
-        respond('New question suggestion', 'A question has been sent for your review.\n' + text, message.guild.members.cache.get('454579681602043916'), 'FF000')
+        respond('New question suggestion', 'A question have been sent for your review.\n' + text, message.guild.members.cache.get('454579681602043916'), 'FF000')
   }catch(error) {
     respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
     errorlog(error)
