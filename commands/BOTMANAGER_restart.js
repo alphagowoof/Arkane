2d31f0b3ad4bcf414c986054cd155e3501a3f9b3
ThatDerpiGuy <61893437+ThatDerpiGuy@users.noreply.github.com>
2020-05-13 01:45:33 -0500

    Fixed an alias for .restart

--- commands/BOTMANAGER_restart.js
+++ commands/BOTMANAGER_restart.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'restart',
 	description: 'Restarts the bot',
-	aliases: ['rebootbot', 'kill', 'reboot', 'power', 'res', 'acpi', 'reboot', 'reset'],
+	aliases: ['rebootbot', 'kill', 'reboot', 'power', 'res', 'acpi', 'rerun', 'reset'],
 	usage: '',
 	cooldown: 0,
 	botmanager:true,
