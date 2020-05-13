53fd9bfe478c03927029a9589fe30b864d38737f
ThatDerpiGuy <61893437+ThatDerpiGuy@users.noreply.github.com>
2020-05-13 15:20:41 -0500

    Update USER_alertsay.js

--- commands/USER_alertsay.js
+++ commands/USER_alertsay.js
@@ -1,15 +1,15 @@
 module.exports = {
   name: 'alertsay',
   aliases: ['alertspeak'],
-  description: 'Has the bot speak.',
+  description: 'Has the bot speak for you.',
   usage: '<text>',
-  cooldown: 60,
+  cooldown: 10,
   mod:false,
   nodelay:true,
 	execute(message, args, client) {	
 		const { prefix } = require('../config.json');
 		const argarray = message.content.slice(prefix.length).trim().split(/ +/g);
 		const text = args.join(' ');
-    message.channel.send(`<@${message.author.id}>` + " says... " + text)
     message.delete()
+    message.channel.send(`<@${message.author.id}>` + " says... " + text)
   }}
