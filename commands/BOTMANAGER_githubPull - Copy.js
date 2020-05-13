55cf6ff1f6b6841cdde95bbecd9c9babde3d5875
ThatDerpiGuy <61893437+ThatDerpiGuy@users.noreply.github.com>
2020-05-13 18:42:12 -0500

    Update BOTMANAGER_githubPull - Copy.js

--- commands/BOTMANAGER_githubPull - Copy.js
+++ commands/BOTMANAGER_githubPull - Copy.js
@@ -1,17 +1,17 @@
 module.exports = {
   name: 'entersafemode',
   aliases: ['safemode'],
-  description: 'Enters safe mode.',
+  description: 'Enters safe mode',
   mod:true,
   botmanager:true,
   essential:true,
 	execute(message, args, client) {
-      try {
+    try {
+      var
       safemode = true
-      });      }catch(error) {
-    respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
+    } catch (error) {
+          respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
     errorlog(error)
     // Your code broke (Leave untouched in most cases)
     console.error('an error has occured', error);
-    }
-    }}
+    }}}
