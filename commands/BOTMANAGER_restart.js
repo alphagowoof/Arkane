c1112b983e976ed1324a88261b98a92ac3b98ea7
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-05-18 23:25:25 -0700

    Added a partial restart and force restart

--- commands/BOTMANAGER_restart.js
+++ commands/BOTMANAGER_restart.js
@@ -9,25 +9,62 @@ module.exports = {
 	essential:true,
 	execute(message, args, client) {
 		try{
-		const fs = require('fs');
-		fs.unlinkSync('./runstate.txt')
-		if (fs.existsSync(`./safe_mode.flag`)){
-			fs.unlinkSync('./safe_mode.flag')
+
+		function fullRestart(){
+			const fs = require('fs');
+			fs.unlinkSync('./runstate.txt')
+			if (fs.existsSync(`./safe_mode.flag`)){
+				fs.unlinkSync('./safe_mode.flag')
+			}
+			if (fs.existsSync(`./errorcount.txt`)){
+			fs.unlinkSync('./errorcount.txt')
+			}
+			console.log("A restart has been triggered. Wait 5 seconds before turning it on again.")
+			const { MessageEmbed } = require('discord.js')
+			const RestartedEmbed = new Discord.MessageEmbed()
+			RestartedEmbed.setTitle('🔄 Restarting')
+			RestartedEmbed.setDescription('Restarting bot and reloading commands, please wait a moment.')
+			message.channel.send(RestartedEmbed)
+			setTimeout(function(){ 
+				console.log("Apple Moderator has been shut down.")
+				console.log("You can now restart it manually, or install PM2 using `npm install pm2 -g`.")
+				process.exit()
+			}, 5000);
 		}
-		if (fs.existsSync(`./errorcount.txt`)){
-		fs.unlinkSync('./errorcount.txt')
+
+		function partialRestart(){
+			respond('',`🧹 Partial restart in progress.`, message.channel)
+			fs.unlinkSync('./runstate.txt')
+			if (fs.existsSync(`./safe_mode.flag`)){
+				fs.unlinkSync('./safe_mode.flag')
+			}
+			if (fs.existsSync(`./errorcount.txt`)){
+			fs.unlinkSync('./errorcount.txt')
+			}
+			client.logout
+			var config = require('../config.json')
+			client.login(config.token)
+			restartedchannel = message.guild.channels.cache.get(config.BotLog)
+			respond('', '✅ Restart completed.', restartedchannel)
+			respond('', '✅ Restart completed.', message.channel)
+
 		}
-		console.log("A restart has been triggered. Wait 5 seconds before turning it on again.")
-		const { MessageEmbed } = require('discord.js')
-		const RestartedEmbed = new Discord.MessageEmbed()
-		RestartedEmbed.setTitle('🔄 Restarting')
-		RestartedEmbed.setDescription('Restarting bot and reloading commands, please wait a moment.')
-		message.channel.send(RestartedEmbed)
-		setTimeout(function(){ 
-			console.log("Apple Moderator has been shut down.")
-			console.log("You can now restart it manually, or install PM2 using `npm install pm2 -g`.")
-			process.exit()
-		}, 5000);
+
+		function forceRestart(){
+			respond('',`ℹ️ Force restart in progress.`, message.channel)
+			setTimeout(function(){ 
+				process.exit()
+			}, 1000);
+		}
+		
+		if(args[0] && args[0].toLowerCase() == 'part' || args[0] && args[0].toLowerCase() == 'partial'){
+			partialRestart()
+		}else if(args[0] && args[0].toLowerCase() == 'f' || args[0] && args[0].toLowerCase() == 'force'){
+			forceRestart()
+		}else{
+			fullRestart()
+		}
+
 	}catch(error) {
 		respond('Error', 'Something went wrong.\n'+error+`\nMessage: ${message}\nArgs: ${args}\n`, message.channel)
 		errorlog(error)
