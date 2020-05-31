a2379db8b5438cd5f1ad71bdc1967b5407845a1e
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-05-31 11:29:27 -0700

    Fix profanity filter

--- ae-bot.js
+++ ae-bot.js
@@ -856,7 +856,7 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
 	const channel = client.channels.cache.get(`${ModLog}`);
 	channel.send(MessageEditEmbed);
 	const profanity = require('./profanity.json');
-	var editedMessage = message.toString().replace(/[^\w\s]/g, "").replace(/\_/g, "")
+	var editedMessage = newMessage.toString().replace(/[^\w\s]/g, "").replace(/\_/, "")
 	var blocked = profanity.filter(word => editedMessage.toLowerCase().includes(word));
 	if (blocked.length > 0) {
 		if(blocked == `${blocked}`)
