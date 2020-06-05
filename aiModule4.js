48964b625983f07b88d32bd26922a974e1e27c7f
Daniel <46201432+DanielC-08@users.noreply.github.com>
2020-06-05 15:17:56 -0700

    Adding missing code back to aiModule4

--- aiModule4.js
+++ aiModule4.js
@@ -41,6 +41,25 @@ module.exports = {
       returnFunction(`idk, go to apple’s website`);
     } else if (input == "volt") {
       returnFunction(`:eyes:`);
-    }
+    }else
+    		//Auto
+		if(input != '' && !fs.existsSync(`./aiModule${this.module+1}.js`)){
+			fs.appendFile('./aiModule_MissingInputs.txt', input+"\n", error => {
+				if(!error){
+					console.log('Added input to missing inputs text file.')
+					returnFunction(`Sorry <@${author.id}>, I don't know how to respond to that...`)
+				} else {
+					console.log('Failed to add input to missing inputs text file.')
+					returnFunction(`Sorry <@${author.id}>, I don't know how to respond to that...`)
+				}
+			})
+		}else 
+		if(fs.existsSync(`./aiModule${this.module+1}.js`)){
+			delete require.cache[require.resolve(`./aiModule${this.module+1}.js`)]
+			aiModule = require(`./aiModule${this.module+1}.js`)
+			aiModule.execute(input, author, returnFunction)
+		}else{
+			return
+		}
   }
 };
