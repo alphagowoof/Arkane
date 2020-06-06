846ce8ab1950e3ae94d41af58da46b518675fd87
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2020-06-05 19:30:26 -0700

    Dev (#21)
    * Update jokes.json
    
    * Add .notice
    
    Added notice.json and commands/USER_notice.js
    
    Co-authored-by: Tommy Stefanos <64721643+tommystefanatic@users.noreply.github.com>
    Co-authored-by: Prestonplayz334.js <66449051+Prestonplayz334-js@users.noreply.github.com>

---
+++ commands/USER_notice.js
@@ -0,0 +1,34 @@
+module.exports = {
+  name: 'notice',
+  aliases: ['none'],
+  description: 'Sends a notice.'
+  usage: '<notice number>',
+  cooldown: 5,
+  disable:false
+  execute(message, args, client) {
+    const Discord = require('discord.js');
+    const fs = require('fs')
+    const {prefix, token} = require('../config.json'); 
+    const argarray = message.content.slice(prefix.length).trim().split(/ +/g);
+    const rulenumber = argarray[1]
+    var entry = rulenumber;
+    var dict = require("../notice.json");
+    var result = dict['Notice'+argarray[1]];
+    if(`${result}` == 'undefined'){respond('', 'Please make sure you entered a valid notice number.',message.channel);return;}
+            try {
+              const RuleEmbed = new Discord.MessageEmbed()
+              .setTitle('about Notice '+argarray[1])
+              .setDescription(`${result}`)
+              .setTimestamp()
+              message.channel.send(RuleEmbed);
+              return;
+            }catch(error) {
+              respond('Error', 'Something went wrong.\n'+error+`\nMessage:${Message}\nArgs: ${args}\n`, message.channel)
+              errorlog(error)
+              //your code broke (leave untouched in most cases)
+            console.error('an error has occured', error);
+            }
+            
+  }
+
+}
